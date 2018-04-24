b8107cd10a8d5f8e15621e0b620d30ed7b1f6cf3
achuDk <achudk@qq.com>
2018-04-24 19:04:46 +0800

    4

--- app/models.py
+++ app/models.py
@@ -10,7 +10,7 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 
 db = SQLAlchemy(app)
 
-# 会员数据模型
+# 会员数据表结构
 class User(db.Model):
     __tablename__ = "user"
     id = db.Column(db.Integer,primary_key=True)     # 编号
@@ -22,4 +22,17 @@ class User(db.Model):
     face = db.Column(db.String(255),unique=True)    # 头像
     addtime = db.Column(db.DateTime,unique=True,default=datetime.utcnow)    # 注册时间
     uuid = db.Column(db.String(255),unique=True)    # 唯一标识符
+    userlogs = db.relationship("Userlog",backref="user") # 会员日志外键关系关联
 
+    def __repr__(self):
+        return "User %r" %self.name
+
+# 登录日志数据表结构
+class Userlog(db.Model):
+    id = db.Column(db.Integer,primary_key=True)             # 编号
+    user = db.Column(db.Integer,db.ForeignKey('user.id'))   # 所属会员
+    ip = db.Column(db.String(100))                          # 登录IP地址
+    addtime = db.Column(db.DataTime,unique=True,default=datatime.utcnow)    #登录时间
+
+    def __repr__(self):
+        return "Userlog %r" %self.id
